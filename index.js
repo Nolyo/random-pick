4dfbf82ec9a6fa9f2b099e057f902eb0e96a130b
Nolyo <yohann.jaffres@nextdecade.io>
2022-11-06 13:58:23 +0100

    Hook only (#1)
    Hook only

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const botSettings = require("./botsettings.json");
+const intents = new Discord.IntentsBitField(3276799);
+const bot = new Discord.Client({intents});
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+const {WebhookClient} = require("discord.js");
+const cron = require("node-cron");
+require('dotenv').config();
 
-client.on('message', msg => {
-    const message = msg.content.split(' ');
-    if (!message.length) return;
-    const commandName = message.shift();
-    if (commandName === '/daily') {
-        const countPeople = message.length;
-        const ordered = [];
-        
-        for (let i = 0; i < countPeople; i++) {
-            const randomIndex = Math.floor(Math.random() * message.length);
-            const item = message[randomIndex];
-            message.splice(randomIndex, 1);
-            ordered.push(item);
-            console.log(randomIndex);
-        }
+bot.login(process.env.TOKEN_WEBHOOK);
 
+function randomIntFromInterval(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-        msg.reply(`Order for the daily (${countPeople} persons: ${ordered.join(', ')}`);
-    }
-});
+function makeEmbed(members) {
+	const countPeople = members.length;
+	/** @type {APIEmbedField[]}*/
+	const ordered = [];
 
-client.login(botSettings.token);
+	for (let i = 0; i < countPeople; i++) {
+		const randomIndex = randomIntFromInterval(0, members.length - 1);
+		const nameMember = members[randomIndex];
+		members.splice(randomIndex, 1);
+		ordered.push({name: `Position ${i + 1}`, value: nameMember, inline: true});
+	}
 
+	const exampleEmbed = {
+		color: 0x0099ff,
+		title: 'Daily Order',
+		url: 'https://meet.google.com/voh-sbqp-kcz',
+		author: {
+			name: 'Daily Bot',
+			url: 'https://meet.google.com/voh-sbqp-kcz',
+		},
+		description: 'The daily will be start',
+		thumbnail: {
+			url: 'https://i.imgur.com/qS99IiV.png',
+		},
+		fields: ordered,
+
+		timestamp: new Date().toISOString(),
+		footer: {
+			text: 'See you soon !',
+		},
+	};
+
+	return exampleEmbed;
+}
+
+bot.on('ready', async () => {
+	try {
+		const webhook = new WebhookClient({
+			url: process.env.URL_WEBHOOK
+		});
+
+		// 55 11 * * Mon-Fri || * * * * *
+		await cron.schedule('* * * * *', () => {
+			webhook.send({embeds: [makeEmbed(['Hugo', 'William', 'Yohann'])]});
+		});
+	} catch (e) {
+		console.error(e);
+	}
+})
