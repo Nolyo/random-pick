a0648dc1ef4822c33ad58d7b7c94066f260509d6
Yohann <yohannjaffres88@gmail.com>
2024-02-14 17:19:22 +0100

    remove useless comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import Discord, { WebhookClient, Events } from "discord.js";
 import { scheduleJobs } from "./utils.js";
-// eslint-disable-next-line
 import * as data from "./config.json" assert { type: "json" };
 
 const intents = new Discord.IntentsBitField(3276799);
