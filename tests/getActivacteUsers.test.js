cf843b50d5f119048b52ce767f4022252037ba68
Nolyo <yohannjaffres88@gmail.com>
2024-02-14 14:46:52 +0100

    add first tests (#2)

---
+++ tests/getActivacteUsers.test.js
@@ -0,0 +1,24 @@
+import { getActivedUsers } from "../src/utils";
+
+test("Return correct filtered list of activated users", () => {
+  const users = [
+    { name: "John", activated: "true" },
+    { name: "Doe", activated: "false" },
+    { name: "Foo", activated: "true" },
+    { name: "Bar", activated: "false" },
+  ];
+
+  const result = getActivedUsers(users);
+  expect(result).toEqual(["John", "Foo"]);
+});
+
+test("Return correct filtered list of activated users", () => {
+  const users = [
+    { activated: "true" },
+    { name: "Doe", activated: "false" },
+    { name: "John", activated: "true" },
+  ];
+
+  const result = getActivedUsers(users);
+  expect(result).toEqual(["John"]);
+});
